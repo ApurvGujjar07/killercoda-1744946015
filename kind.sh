2b3358d607ba2c11c5ec3e7ab100f32ccf49b80d
Gujjar Apurv <gujjarapurv181@gmail.com>
2025-04-20 08:38:20 +0530

    Create kind.sh

---
+++ kind.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Download and install kind
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.27.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+# Download and install kubectl
+VERSION="v1.30.0"
+URL="https://dl.k8s.io/release/${VERSION}/bin/linux/amd64/kubectl"
+INSTALL_DIR="/usr/local/bin"
+
+curl -LO "$URL"
+chmod +x kubectl
+sudo mv kubectl $INSTALL_DIR/
+kubectl version --client
+
+# Remove temporary files
+rm -f kubectl
+rm -rf kind
+echo "kind & kubectl installation complete."
